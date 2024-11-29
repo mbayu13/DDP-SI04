734a25a50f609dc739915d42622c3a3dbcac41b1
mbayu13 <anggarambayu572@gmail.com>
2024-11-29 21:38:51 +0700

    Add files via upload

---
+++ bangundatar.py
@@ -0,0 +1,10 @@
+import main
+
+main.l_persegi(10)
+main.persegi_panjang(10,2)
+main.l_segitiga(12,10)
+main.belahketupat(12,2)
+main.layanglayang(8,4)
+
+
+
---
+++ bangunruang.py
@@ -0,0 +1,9 @@
+import main
+
+print("## Modul Luas Bangun Ruang ##")
+
+main.balok(4,8,10)
+main.l_kubus(7)
+main.luastabung(14,9)
+main.prismasegitiga(5,10,15)
+main.limassegiempat(4,10)
